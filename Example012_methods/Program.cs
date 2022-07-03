500354d20cab69a4dca918344eea42d1c9f51aaf
SERGY <kogo_sadgi@mail.ru>
2022-07-03 17:16:07 +0300

    Example 012 Methods made changes

--- Example012_methods/Program.cs
+++ Example012_methods/Program.cs
@@ -98,13 +98,116 @@
 
 
 //метод использования цикла в цикле
-for (int i = 2; i <= 10; i++)
+// for (int i = 2; i <= 10; i++)
+// {
+//     //j-используем для внутренего счетчика
+//     for (int j = 2; j <= 10; j++)
+//     {
+//                         //интраполяция строк
+//         Console.WriteLine($"{i} x {j} = {i*j}");
+//     }
+//     Console.WriteLine();
+// }
+
+
+
+
+//Дан текст. В тексте нужно все пробелы заменить черточками, 
+//маленькие буквы “к” заменить большими “К”, а большие “С” заменить 
+//маленькими “с”.
+
+// string text= "— Я думаю, — сказал князь, улыбаясь, — что, " 
+//            + "ежели бы вас послали вместо нашего милого "
+//            + "Винценгероде, вы бы взяли приступом согласие "
+//            + "прусского короля. Вы так красноречивы. Вы "
+//            + "дадите мне чаю?";
+
+
+
+// string s = "qwerty"
+//             012345
+// s[3] // r
+
+// string Replace(string text, char oldValue, char newValue)
+// {
+//     string result = string.Empty;
+
+//     int length = text.Length;
+//     for (int i = 0; i < length; i++)
+//     {
+//         if(text[i] == oldValue) result = result + $"{newValue}";
+//         else result = result + $"{text[i]}";
+//     }
+
+//     return result;
+// }
+
+// string newText = Replace(text, ' ', '|');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(newText, 'к', 'К');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(newText, 'С', 'с');
+// Console.WriteLine(newText);
+
+
+// Replace             Заменить
+// string              строку
+// WriteLine           Линия записи
+// length              длина
+// char old Value      символа старое значение
+// char newValue       символа Новое значение
+
+
+
+
+// 1. Найти позицию минимального элемента
+// в неотсортированной части массива
+// 2. Произвести обмен этого значения со 
+// значением первой неотсортированной позиции
+// 3. Повторять пока есть не отсортированные 
+// элементы
+
+// Задачи
+// Максимум в массиве
+// Минимум в массиве
+// Максимум в упорядоченном массиве
+// Подсчет цифр числа
+
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray(int[] array)
 {
-    //j-используем для внутренего счетчика
-    for (int j = 2; j <= 10; j++)
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
     {
-                        //интраполяция строк
-        Console.WriteLine($"{i} x {j} = {i*j}");
+        Console.Write($"{array[i]} ");
     }
     Console.WriteLine();
 }
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)//
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition])
+        {
+    minPosition = j;
+    }
+ }
+ int temporary = array[i];
+ array[i] = array[minPosition];
+ array[minPosition] = temporary;
+ }
+}
+
+
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
